0f9cbc29f0500bf8770c042a0370876412b811d1
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-23 16:18:27 +0100

    Refactors to use two methods - incrementHandler() and decrementHandler()

--- src/App.js
+++ src/App.js
@@ -9,19 +9,28 @@ class App extends Component {
     this.state = {
       counter: 0
     }
+
+    this.incrementHandler = this.incrementHandler.bind(this);
+    this.decrementHandler = this.decrementHandler.bind(this);
+  }
+
+  incrementHandler() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  decrementHandler() {
+    this.setState({ counter: this.state.counter - 1 });
   }
 
   render() {
   return (
     <div data-test="component-app">
-      <h1>App</h1>
-      <button onClick={() => this.setState({ counter: this.state.counter + 1})} data-test="increment-button">I am an increment button</button>
-      <button onClick={() => this.setState({ counter: this.state.counter - 1})} data-test="decrement-button">I am a decrement button</button>
+      <button onClick={this.incrementHandler} data-test="increment-button">I am an increment button</button>
+      <button onClick={this.decrementHandler} data-test="decrement-button">I am a decrement</button>
       <h2 data-test="counterDisplay">{this.state.counter}</h2>
     </div>
   );
 }
-
-  }
+}
 
 export default App;
