f28b01358ddc5ba2f3567a6404d9b477f76c04cb
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-23 12:30:37 +0100

    Adds the findByTestAttribute() method to each test to DRY up code - replacement

--- src/App.test.js
+++ src/App.test.js
@@ -6,18 +6,6 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-/**
- * Return ShallowWrapper containing node(s) with the given data-test value
- * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search with
- * @param { string } val - Value of data-test attribute
- * @returns {ShallowWrapper}
- */
-
- 
- const findByTestAttribute = (wrapper, val) => {
-   return wrapper.find(`[data-test=${val}]`);
- };
-
  /** 
  * Factory function to create a ShallowWrapper for the App component.
  * @function setup
@@ -31,21 +19,35 @@ const setup = (props={}, state=null) => {
 }
 
 
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search with
+ * @param { string } val - Value of data-test attribute
+ * @returns {ShallowWrapper}
+ */
+
+ 
+ const findByTestAttribute = (wrapper, val) => {
+   return wrapper.find(`[data-test="${val}"]`);
+ }
+
+
+
 test('it renders without an error', () => {
   const wrapper = setup();
-  const appComponent  = wrapper.find("[data-test='component-app']")
+  const appComponent = findByTestAttribute(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
 test('it renders the increment button', () => {
   const wrapper = setup();
-  const button = wrapper.find("[data-test='increment-button']")
+  const button = findByTestAttribute(wrapper, 'increment-button')
   expect(button.length).toBe(1);
 });
 
 test('it renders the display counter', () => {
-  const wrapper =setup();
-  const counterDisplay = wrapper.find("[data-test='counterDisplay']")
+  const wrapper = setup();
+  const counterDisplay = findByTestAttribute(wrapper, 'counterDisplay' )
   expect(counterDisplay.length).toBe(1);
 })
 
